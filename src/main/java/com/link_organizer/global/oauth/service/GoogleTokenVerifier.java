323ecd8071ecbc22f0d1067137b5d909f5ddae76
Won-you <cd2342@naver.com>
2026-04-28 22:38:13 +0900

    google client-id 추가

--- src/main/java/com/link_organizer/global/oauth/service/GoogleTokenVerifier.java
+++ src/main/java/com/link_organizer/global/oauth/service/GoogleTokenVerifier.java
@@ -4,7 +4,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
-import java.util.Collections;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -15,10 +15,11 @@ public class GoogleTokenVerifier {
 
     private final GoogleIdTokenVerifier verifier;
 
-    public GoogleTokenVerifier(@Value("${google.client-id}") String clientId) {
-//        log.info("google : {}",clientId);
+    public GoogleTokenVerifier(@Value("${google.client-id.ios}") String iosClientId,
+        @Value("${google.client-id.android}") String androidClientId,
+        @Value("${google.client-id.web}") String webClientId) {
         this.verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), GsonFactory.getDefaultInstance())
-                .setAudience(Collections.singletonList(clientId))
+                .setAudience(List.of(iosClientId, androidClientId, webClientId))
                 .build();
     }
 
